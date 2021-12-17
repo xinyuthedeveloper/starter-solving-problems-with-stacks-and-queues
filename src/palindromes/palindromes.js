38f7200b3c6875a716e252a5b3452e1f0e368a39
Xin Yu <wilsonyu0626@gmail.com>
2021-12-17 13:34:19 -0800

    implement palindromes algo

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -3,6 +3,22 @@ const Stack = require("../lib/stack");
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
   // Your solution here
+  let middle = Math.floor(sentence.length / 2);
+  const stack = new Stack();
+
+  for (let i = 0; i < middle; i++) {
+    stack.push(sentence[i]);
+  }
+
+  middle += sentence.length % 2 === 0 ? 0 : 1;
+
+  for (let i = middle; i < sentence.length; i++) {
+    if (sentence[i] !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 module.exports = isPalindrome;
