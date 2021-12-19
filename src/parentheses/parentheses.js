6e26a6f077911c54b00d0ab28a2e34490a545259
Xin Yu <wilsonyu0626@gmail.com>
2021-12-19 13:01:14 -0800

    practice

--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,22 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+    const stack = new Stack();
+    
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] === "(") {
+            stack.push(expression[i]);
+        } else {
+            if (expression[i] === ")") {
+                if (stack.top) {
+                    stack.pop()
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return !stack.top;
+};
 
 module.exports = match;
--- src/postfix/postfix.js
+++ src/postfix/postfix.js
@@ -1,5 +1,58 @@
 const Stack = require("../lib/stack");
 
-const postfix = (expression) => {};
+const precedence = {
+  "+": 0,
+  "-": 0,
+  "*": 1,
+  "/": 1,
+};
+
+const postfix = (expression) => {
+
+  const stack = new Stack();
+  const result = [];
+
+  expression = expression.replace(/\s/g, "");
+
+  expression.split("").forEach((character) => {
+    if (character === "(") {
+      stack.push(character);
+    } else {
+      if (character === ")") {
+        let top = stack.pop();
+        while (top !== "(") {
+          result.push(top);
+          top = stack.pop();
+        }
+      } else {
+        if ("+-*/".includes(character)) {
+          if (
+            !stack.top ||
+            stack.top.value === "(" ||
+            precedence[character] > precedence[stack.top.value]
+          ) {
+            stack.push(character);
+          } else {
+            while (stack.top && precedence[stack.top.value] >= precedence[character]) {
+              result.push(stack.pop());
+            }
+
+            stack.push(character);
+          }
+        } else {
+          result.push(character);
+        }
+      }
+    }
+  });
+
+  while (stack.top) {
+    result.push(stack.pop());
+  }
+
+  return result.join(" ");
+};
+
+console.log(postfix("(((a + b) * (c - d))/(a - b) + (c / d))"));
 
 module.exports = postfix;
